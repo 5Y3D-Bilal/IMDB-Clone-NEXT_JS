3cfe3738a4b4a009bfe9cbcfd972c41a25bb77f9
Bilal567 <140496107+5Y3D-Bilal@users.noreply.github.com>
2024-02-03 22:13:20 +0500

    About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 flex flex-col">
+      <h1 className="text-2xl  font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eveniet
+        debitis fugit quis nobis a fuga, tempore autem pariatur eligendi. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Aliquid dicta itaque
+        impedit nemo harum pariatur fugiat libero tempore laudantium! Pariatur
+        dolorem officiis sequi perferendis nobis corporis atque culpa a
+        repellat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        cumque nesciunt dignissimos libero, reiciendis enim, fugiat odit alias
+        voluptatem nisi deleniti esse. Suscipit animi ducimus, pariatur quo
+        explicabo blanditiis repudiandae. Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Sit illo, iure quas maiores amet nobis!
+        Saepe aliquid possimus id neque doloribus nihil, cupiditate velit
+        consequuntur recusandae sed eum quae sint.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam a
+        ducimus consequuntur vitae expedita aspernatur possimus esse dolorem
+        necessitatibus vel ipsa nobis autem quos ullam labore corrupti ea maxime
+        voluptatem sapiente unde, perspiciatis quidem velit recusandae aliquam.
+        Veniam cumque hic suscipit, voluptate accusantium quia, qui delectus
+        eaque quam ipsum praesentium.
+      </p>
+    </div>
+  );
+}
